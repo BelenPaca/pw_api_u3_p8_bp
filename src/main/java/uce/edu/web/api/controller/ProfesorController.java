5de27bb36361283242164f0b262682d207279df8
BelenPaca <bapaca@uce.edu.ec>
2025-06-25 22:12:36 -0500

    Tarea 08 - Aplicación del Modelo de Madurez de Richardson Nivel 0 y 1 a Profesores

--- src/main/java/uce/edu/web/api/controller/ProfesorController.java
+++ src/main/java/uce/edu/web/api/controller/ProfesorController.java
@@ -1,7 +1,15 @@
 package uce.edu.web.api.controller;
 
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.repository.modelo.Profesor;
@@ -14,10 +22,39 @@ public class ProfesorController {
     private IProfesorService profesorService;
 
     @GET
-    @Path("/consultar/{id}")
-    public Profesor consultarPorId (@PathParam ("id") Integer id){
+    @Path("/{id}")
+    public Profesor consultarPorId(@PathParam("id") Integer id) {
         return this.profesorService.buscarPorID(id);
 
+    }
+
+    @GET
+    @Path("")
+    public List<Profesor> consultarTodos() {
+        return this.profesorService.buscarTodos();
+    }
+
+    @POST
+    @Path("")
+    public void guardar(@RequestBody Profesor profesor) {
+    }
+
+    @PUT
+    @Path("/{id}")
+    public void actualizarPorId (Profesor profesor, @PathParam("id") Integer id){
+
+    }
+
+    @PATCH
+    @Path("/{id}")
+    public void actualizarParcialPorId (@RequestBody Profesor profesor, @PathParam("id") Integer id){
+
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public void borrarPorId (@PathParam ("id") Integer id) {
+        
     }
 
 }
